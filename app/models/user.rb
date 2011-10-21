1737488a9be2fd39a3c1c2649da14b43fc791885
Kirill Lashuk <kirill.lashuk@gmail.com>
2011-10-21 23:12:05 +0300

    Add email validation to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,11 @@ class User
 
   field :name
 
-  validates_presence_of :name
-  validates_uniqueness_of :name, :email, :case_sensitive => false
+  validates_presence_of   :name
+  validates_uniqueness_of :name, :case_sensitive => false
+
+  validates_uniqueness_of :email, :case_sensitive => false, :allow_blank => true
+  validates_format_of     :email, :with => Devise::email_regexp, :allow_blank => true, :if => :email_changed?
 
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
