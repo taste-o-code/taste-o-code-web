387e910633e8ba879bbdb644d71c5910377cf703
Kirill Lashuk <kirill.lashuk@gmail.com>
2011-10-21 23:17:15 +0300

    Add password length validation to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User
   validates_uniqueness_of :email, :case_sensitive => false, :allow_blank => true
   validates_format_of     :email, :with => Devise::email_regexp, :allow_blank => true, :if => :email_changed?
 
+  validates_length_of :password, :within => Devise::password_length, :allow_blank => true
+
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
 end
