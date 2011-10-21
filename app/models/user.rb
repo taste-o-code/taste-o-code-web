678b613add63c749f7d6728001d091ed4f65c0a5
Kirill Lashuk <kirill.lashuk@gmail.com>
2011-10-21 23:02:10 +0300

    Remove Devise validatable module from User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User
+
   include Mongoid::Document
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable
 
   field :name
+
   validates_presence_of :name
   validates_uniqueness_of :name, :email, :case_sensitive => false
+
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
 end
