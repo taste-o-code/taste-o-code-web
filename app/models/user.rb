a49fc2df34bd226debee345ded9a8ca807c1af39
Nikita Beloglazov <nikelandjelo@gmail.com>
2011-10-22 13:16:27 +0300

    Simplify code.

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,29 +31,34 @@ class User
 
   validates_length_of :password, :within => Devise::password_length, :allow_blank => true
 
-  def self.find_by_identity_url(identity_url)
-    User.where(:open_id_identities.matches => {:identity_url => identity_url}).first
+  # Class methods
+  class << self 
+    def find_by_identity_url(identity_url)
+      User.where(:open_id_identities.matches => {:identity_url => identity_url}).first
+    end
+
+    def build_from_identity_url(identity_url)
+      user = User.new
+      user.open_id_identities << OpenIdIdentity.new(:identity_url => identity_url)
+      return user
+    end
+
+    def openid_required_fields
+      [AX_FIRST_NAME, AX_LAST_NAME, AX_EMAIL, FULLNAME, NICKNAME, EMAIL]
+    end
   end
 
-  def self.build_from_identity_url(identity_url)
-    user = User.new
-    user.open_id_identities << OpenIdIdentity.new(:identity_url => identity_url)
-    return user
-  end
-
-  def self.openid_required_fields
-    [AX_FIRST_NAME, AX_LAST_NAME, AX_EMAIL, FULLNAME, NICKNAME, EMAIL]
-  end
-
+  # Instance methods
   def openid_fields=(fields)
     # Some AX providers can return multiple values per key
     # Leave only first element from all arrays.
     fields = fields.inject({}) do |h, (k,v)|
       h[k] = v.is_a?(Array) ? v.first : v
       h
-    end
-    self.name = get_name_from_openid(fields)
-    self.email = get_email_from_openid(fields)
+    end      
+
+    self.name = fields[NICKNAME] || fields[FULLNAME] || [fields[AX_FIRST_NAME], fields[AX_LAST_NAME]].compact.join(' ')
+    self.email = fields[EMAIL] || fields[AX_EMAIL]
   end
 
   def identity_url
@@ -63,22 +68,6 @@ class User
   def gravatar
     hash = Digest::MD5.hexdigest(self.email.strip.downcase)
     return "http://www.gravatar.com/avatar/#{hash}?s=40"
-  end
-
-  private 
-  
-  def get_name_from_openid(fields)
-    if fields.has_key?(NICKNAME)
-      fields[NICKNAME]
-    elsif fields.has_key?(FULLNAME)
-      fields[FULLNAME]
-    elsif fields.has_key?(AX_FIRST_NAME) || fields.has_key?(AX_LAST_NAME)
-      [fields[AX_FIRST_NAME], fields[AX_LAST_NAME]].compact.join(' ')
-    end
-  end
-
-  def get_email_from_openid(fields)
-    fields[EMAIL] || fields[AX_EMAIL]
   end
 
 end
