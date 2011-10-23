0261c1992227059d0c0015dee9eaff57ea227b2d
Nikita Beloglazov <nikelandjelo@gmail.com>
2011-10-23 03:03:07 +0300

    Fix devise controllers namespace bug.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
   NAVIGATION_ITEMS = {
-      'home'      => { :controller => :home,      :action => :show },
-      'users'     => { :controller => :users,     :action => :index },
-      'languages' => { :controller => :languages, :action => :index },
-      'about'     => { :controller => :about,     :action => :show },
+      'home'      => { :controller => '/home',      :action => :show },
+      'users'     => { :controller => '/users',     :action => :index },
+      'languages' => { :controller => '/languages', :action => :index },
+      'about'     => { :controller => '/about',     :action => :show },
   }
 
   def navigation
