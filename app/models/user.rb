5ec94c84c93508a84bb27c9d058406bdf9e8d30d
Kirill Lashuk <kirill.lashuk@gmail.com>
2011-10-29 12:49:29 +0300

    Some meaningless reformatting.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,29 +3,31 @@ class User
 
   include Mongoid::Document
 
-  AX_FIRST_NAME = 'http://axschema.org/namePerson/first'
-  AX_LAST_NAME =  'http://axschema.org/namePerson/last'
-  AX_EMAIL = 'http://axschema.org/contact/email'
-  EMAIL = 'email'
-  FULLNAME = 'fullname'
-  NICKNAME = 'nickname'
+  field :name
+  field :fullname
+  field :location
+
+  embeds_many :open_id_identities
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :openid_authenticatable
 
-  field :name
-  field :fullname
-  field :location
+  AX_FIRST_NAME = 'http://axschema.org/namePerson/first'
+  AX_LAST_NAME  = 'http://axschema.org/namePerson/last'
+  AX_EMAIL      = 'http://axschema.org/contact/email'
+  EMAIL         = 'email'
+  FULLNAME      = 'fullname'
+  NICKNAME      = 'nickname'
 
   attr_accessible :name, :email, :password, :password_confirmation,
                   :remember_me, :fullname, :location, :login
+
   attr_writer :identity_url
+
   # Virtual attribute for authenticating by either name or email
   # This is in addition to a real persisted field like 'name'
   attr_accessor :login
-
-  embeds_many :open_id_identities
 
   validates_presence_of   :name
   validates_uniqueness_of :name, :case_sensitive => false
@@ -36,31 +38,35 @@ class User
 
   validates_length_of :password, :within => Devise::password_length, :allow_blank => true
 
-  # Class methods
+
   class << self
+
     def find_by_identity_url(identity_url)
-      User.where(:open_id_identities.matches => {:identity_url => identity_url}).first
+      User.where(:open_id_identities.matches => { :identity_url => identity_url }).first
     end
 
     def build_from_identity_url(identity_url)
       user = User.new
       user.open_id_identities << OpenIdIdentity.new(:identity_url => identity_url)
-      return user
+      user
     end
 
     def openid_required_fields
       [AX_FIRST_NAME, AX_LAST_NAME, AX_EMAIL, FULLNAME, NICKNAME, EMAIL]
     end
+
+    def find_for_database_authentication(conditions)
+      login = conditions.delete(:login)
+      self.any_of({ :name => login }, { :email => login }).first
+    end
+
   end
 
-  # Instance methods
+
   def openid_fields=(fields)
     # Some AX providers can return multiple values per key.
     # Leave only first element from all arrays.
-    fields = fields.inject({}) do |h, (k,v)|
-      h[k] = v.is_a?(Array) ? v.first : v
-      h
-    end
+    fields = fields.inject({}) { |h, (k,v)| h.merge k => v.is_a?(Array) ? v.first : v }
 
     self.name ||= fields[NICKNAME] || fields[FULLNAME] || [fields[AX_FIRST_NAME], fields[AX_LAST_NAME]].compact.join(' ')
     self.email = fields[EMAIL] || fields[AX_EMAIL]
@@ -72,7 +78,7 @@ class User
 
   def gravatar(size)
     hash = Digest::MD5.hexdigest(self.email.strip.downcase)
-    return "http://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    "http://www.gravatar.com/avatar/#{hash}?s=#{size}"
   end
 
   def update_with_password(params={})
@@ -85,12 +91,12 @@ class User
 
     # Check current password only when user changes password.
     result = if params[:password].blank? or valid_password?(current_password)
-        update_attributes(params)
+      update_attributes(params)
     else
-        self.attributes = params
-        self.valid?
-        self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
-        false
+      self.attributes = params
+      self.valid?
+      self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+      false
     end
 
     clean_up_passwords
@@ -103,13 +109,6 @@ class User
 
   def to_param
     name
-  end
-
-  protected
-
-  def self.find_for_database_authentication(conditions)
-    login = conditions.delete(:login)
-    self.any_of({ :name => login }, { :email => login }).first
   end
 
 end
