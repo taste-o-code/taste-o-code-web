db0119e2da57232035c84e54582f7dbd40ef663b
Nikita Beloglazov <nikelandjelo@gmail.com>
2011-10-31 01:56:42 +0300

    Forgot to remove logging :(

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    logger.info "In update method"
     if @user.update_attributes(params[:user])
       redirect_to :action => :show, :id => params[:id]
     else
