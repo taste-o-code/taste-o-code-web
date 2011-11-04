715702405393f05805743b5f23dec9324d79891d
Nikita Beloglazov <nikelandjelo@gmail.com>
2011-11-05 01:21:29 +0300

    Add redirecting user to last visited page after login. Close #17.

--- app/controllers/omniauth_authentications_controller.rb
+++ app/controllers/omniauth_authentications_controller.rb
@@ -3,23 +3,25 @@ class OmniauthAuthenticationsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:create]
 
   def create
-    omniauth = request.env["omniauth.auth"]
+    omniauth = request.env['omniauth.auth']
     user = User.where(:authentications.matches => {:provider => omniauth['provider'],
                                                    :uid => omniauth['uid']}).first
-
+    back_url = request.env['omniauth.origin'] || :root
     if user
       flash[:notice] = "Signed in successfully."
-      sign_in_and_redirect(:user, user)
+      sign_in(:user, user)
+      redirect_to back_url
     elsif user_signed_in?
       current_user.authentications.create(:provider => omniauth['provider'], :uid => omniauth['uid'])
       flash[:notice] = "Authentication successful."
-      redirect_to :root
+      redirect_to back_url
     else
       user = User.new
       user.apply_omniauth(omniauth)
       if user.save
         flash[:notice] = "Signed in successfully."
-        sign_in_and_redirect(:user, user)
+        sign_in(:user, user)
+        redirect_to back_url
       else
         msg = "Email #{omniauth['user_info']['email']} is already taken. " +
           "In case it is your account, try to login and add facebook option in settings."
