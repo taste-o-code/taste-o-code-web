dc14f2f2f46db60974dfa3f61d5a25e7a441723e
Kirill Lashuk <kirill.lashuk@gmail.com>
2011-11-07 16:02:00 +0300

    Minor edits to UsersController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,14 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id].to_i)
+    unless @user = User.first(:conditions => { :id => params[:id].to_i })
+      redirect_to({ :action => :index }, :alert => "There is no user with id #{params[:id]}.")
+    end
   end
 
   def update
     if @user.update_attributes(params[:user])
+      flash[:notice] = 'Your profile successfully updated.'
       render :action => :show
     else
       render :action => :edit
@@ -25,7 +28,7 @@ class UsersController < ApplicationController
     if params[:id] == current_user.id.to_s
       @user = current_user
     else
-      redirect_to :action => :show, :id => params[:id]
+      redirect_to({ :action => :show, :id => params[:id] }, :alert => 'You can edit only your own profile.')
     end
   end
 
