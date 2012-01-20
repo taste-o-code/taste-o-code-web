58d83ea7bce90c3a8539fa083d136efdd33ee52f
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-01-21 02:29:29 +0300

    Set server ip as host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ TasteOCodeWeb::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'taste-o-code.com' }
+  config.action_mailer.default_url_options = { :host => '178.79.183.55' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
