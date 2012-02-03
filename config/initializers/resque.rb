60aa300db762f1d8b7ddd9e3a3b3eb3a5b77a8d5
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-02-04 02:49:26 +0300

    Move resque config to yaml file.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,10 +1,4 @@
-Rails.configuration.resque = {
-  :redis_resque => 'localhost:6379',
-  :redis_pyres => 'localhost:6380',
-  :queue_resque => 'submission_results',
-  :queue_pyres => 'submissions',
-  :worker_pyres => 'worker.SubmissionChecker'
-}
+Rails.configuration.resque = YAML.load_file("#{Rails.root.to_s}/config/resque.yml").symbolize_keys
 
 # Use :redis_pyres because rails app enqueues jobs for pyres.
 # So it need to use redis for pyres.
