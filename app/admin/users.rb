4d536ee434e93aa065e1d63fbfe1184f0c57bf4c
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-02-12 20:51:01 +0300

    Setup user editing.

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,11 @@
 ActiveAdmin.register User do
+
+  controller do
+    def resource
+      @user ||= User.find params[:id].to_i
+    end
+  end
+
   index do
     column :id
     column :name
@@ -8,5 +15,6 @@ ActiveAdmin.register User do
     column :languages do |user|
       user.languages.map{ |l| l.name }.join(',')
     end
+    default_actions
   end
 end
