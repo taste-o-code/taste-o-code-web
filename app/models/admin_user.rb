0e1a1e648bb41ab5a0003ef37a221416d26fddcd
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-06-09 01:22:38 +0300

    Migrating to devise 2.0.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,4 +4,22 @@ class AdminUser
 
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
+  ## Database authenticatable
+  field :email,              type: String, null: false
+  field :encrypted_password, type: String, null: false
+
+  ## Recoverable
+  field :reset_password_token,   type: String
+  field :reset_password_sent_at, type: Time
+
+  ## Rememberable
+  field :remember_created_at, type: Time
+
+  ## Trackable
+  field :sign_in_count,      type: Integer
+  field :current_sign_in_at, type: Time
+  field :last_sign_in_at,    type: Time
+  field :current_sign_in_ip, type: String
+  field :last_sign_in_ip,    type: String
+
 end
